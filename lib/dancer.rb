2f44869cee6e37f8854af8b35cf0c6b249406615
Tom Banish <tombanish@gmail.com>
2020-02-18 22:25:53 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -6,10 +6,4 @@ require_relative './fancy_dance.rb'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-  
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
 end
